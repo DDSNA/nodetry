83baea087584df7373e53d56dffccd1b7423e8e2
Dan <79444147+DDSNA@users.noreply.github.com>
2022-09-12 14:00:19 +0300

    added contactmeinfo js + 404 in app.js

--- app.js
+++ app.js
@@ -87,6 +87,18 @@ express()
 
 // }//  
 
+// This route will handle all the requests that are 
+// not handled by any other route handler. In 
+// this hanlder we will redirect the user to 
+// an error page with NOT FOUND message and status
+// code as 404 (HTTP status code for NOT found)
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>404! Page not found</h1>');
+});
+  
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
 
 
 module.exports = app;
