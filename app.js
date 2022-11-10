4e1a344848a53f219bab201259f823bd72c9a75a
Dan <79444147+DDSNA@users.noreply.github.com>
2022-11-10 11:32:02 +0200

    added app.use router to app.js and users.js routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var usersRouter = require('./routes/users');
 
 //added code 
 var fs = require('fs');
@@ -88,7 +89,8 @@ express()
 
 // }//  
 
+// database mysql 
 
-
+app.use('/users', usersRouter);
 module.exports = app;
 
