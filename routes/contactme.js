918d455be18114901e3e280aa76c55228ff64bab
DDSNA <79444147+DDSNA@users.noreply.github.com>
2022-11-16 22:14:55 +0200

    added validation to route; route fails

--- routes/contactme.js
+++ routes/contactme.js
@@ -6,6 +6,11 @@ router
     .route("/contactmeupload")
     .get((req,res) => {
         res.send("hi get /contactme/contactmeupload");
+        req.assert("name", "Name is required").notEmpty()
+        req.assert("email", "Email is required").isEmail()
+
+        var errors = req.validationErrors()
+        
     })
     .post((req,res) => {
         res.send("hi post /contactme/contactmeupload")
