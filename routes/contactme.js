0ae11c4d3a57c52413cff281c90d423818149b15
DDSNA <79444147+DDSNA@users.noreply.github.com>
2022-11-17 22:48:51 +0200

    added error status for get

--- routes/contactme.js
+++ routes/contactme.js
@@ -1,17 +1,21 @@
 "use strict";
 const express = require("express");
+const send = require("send");
 let router = express.Router();
 
 router
     .route("/contactmeupload")
-    .get((req,res) => {
+    .get(async (req,res) => {
         res.send("hi get /contactme/contactmeupload");
         req.assert("name", "Name is required").notEmpty()
         req.assert("email", "Email is required").isEmail()
 
         res.sendFile(__dirname + "/index.html")
-
-        var errors = req.validationErrors()
+        if (err) {
+            res.status(400).send("error fetching name")
+        } else {
+            res.json(result);
+        }
 
     })
     .post((req,res) => {
