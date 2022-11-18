da2ec4de66d1680e8bdd3b32684761b39555ee63
DDSNA <79444147+DDSNA@users.noreply.github.com>
2022-11-18 09:04:19 +0200

    better 404

--- app.js
+++ app.js
@@ -32,12 +32,12 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -53,57 +53,52 @@ express()
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
   .get("/cool", (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
-	// we"ll add code here soon
+// we"ll add code here soon
 /**
  * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
  * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
  */
 
- var MongoClient = require("mongodb").MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 
- // Connect to the db
- var url = "mongodb+srv://weblogin:pVAS41EoXr0J7MRH@webdb.wocgjn8.mongodb.net/test?retryWrites=true&w=majority";
+// Connect to the db
+var url = "mongodb+srv://weblogin:pVAS41EoXr0J7MRH@webdb.wocgjn8.mongodb.net/test?retryWrites=true&w=majority";
 
- MongoClient.connect(url, function (err, db) {
-    
-      if(err) throw err;
-      var dbase = db.db("contactmedata")
-      //dbase.createCollection("contactmeinfo", function (err, res){
-      //  if (err) throw err;
-      console.log("New collection created!");
-      db.close;
-      })
-      //Write databse Insert/Update/Query code here..
-                 
-//async function main() {
-//   const uri = "mongodb+srv://danadmin:1l4gNTYBgOz6vWsr@webdb.wocgjn8.mongodb.net/test?retryWrites=true&w=majority";
-//   const client = new MongoClient(uri);
-//   try {
-//     await client.connect();
+MongoClient.connect(url, function (err, db) {
 
-//     await listDatabases(client);
- 
-//   } catch (e) {
-//     console.error(e);
-//   }
-//   finally {
-//     await client.close();
-//   } 
-
-// }//  
-
+  if (err) throw err;
+  var dbase = db.db("contactmedata")
+  //dbase.createCollection("contactmeinfo", function (err, res){
+  //  if (err) throw err;
+  console.log("Connection to MongoDB succeeded!");
+  db.close;
+})
 // This route will handle all the requests that are 
 // not handled by any other route handler. In 
 // this hanlder we will redirect the user to 
 // an error page with NOT FOUND message and status
 // code as 404 (HTTP status code for NOT found)
 app.all("*", (req, res) => {
-  res.status(404).send("<h1>404! Page not found</h1>");
+  res.status(404);
+
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
 });
-  
+
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
 });
@@ -111,23 +106,9 @@ app.listen(3000, () => {
 
 //added code for dbconn
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, "public")));
 
-var dbConn = MongoClient.connect("mongodb+srv://weblogin:pVAS41EoXr0J7MRH@webdb.wocgjn8.mongodb.net/test?retryWrites=true&w=majority", function (err, db) {
-    
-  if(err) throw err;
-
-});
-
-app.post("/contactmeupload", function (req, res) {
-  var dbase = db.db("contactmedata")
-  dbConn.then(function(db) {
-      delete req.body._id; // for safety reasons
-      db.collection("contactmeinfo").insertOne(req.body);
-  });    
-  res.send("Data received:\n" + JSON.stringify(req.body));
-});
 
 //contactme 
 
